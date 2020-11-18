afd1e08b750841fb558e463ba14f4156d1925a42
Alexis Montilla <alexisamm9261@gmail.com>
2020-11-17 20:11:47 -0400

    h

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <!-- CSRF Token -->
@@ -11,15 +11,16 @@
     <title>@yield('title')</title>
 
     {{-- Styles --}}
-    <link rel="stylesheet" href="{{ asset('css/vendor.css') }}"/>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('app/assets/css/vendor.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('app/assets/css/style.css') }}"/>
+
 </head>
 <body>
-    <main class="py-4">
+    <main>
         @yield('content')
     </main>
 
-    <script src="{{ asset('js/vendor.js') }}"></script>
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('app/assets/js/vendor.js') }}"></script>
+    <script src="{{ asset('app/assets/js/app.js') }}"></script>
 </body>
 </html>
